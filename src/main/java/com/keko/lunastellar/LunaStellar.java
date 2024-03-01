399e9761aed5c099f66819877ecab6c2c8e7d852
Salsa <andrei.pencu@outlook.com>
2024-03-01 18:24:37 +0200

    Setup

---
+++ src/main/java/com/keko/lunastellar/LunaStellar.java
@@ -0,0 +1,16 @@
+package com.keko.lunastellar;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LunaStellar implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("luna");
+	public static final String MOD_ID = "lunastellar";
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		LOGGER.info("Bow before the stars (Cringe af log ngl)", mod.metadata().name());
+	}
+}
